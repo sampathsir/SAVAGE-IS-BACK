3b6add83893614911d680ea19f2bcb09e7096957
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-03-17 08:49:40 +0530

    Update and rename lightning.py to savage.py

--- userbot/savage.py
+++ userbot/savage.py
@@ -5,18 +5,18 @@
 #chala jaa bhosdike 
 import os
 from userbot import ALIVE_NAME 
-MASTER = str(ALIVE_NAME) if ALIVE_NAME else "LIGHTNING"
+MASTER = str(ALIVE_NAME) if ALIVE_NAME else "SAVAGE BOY"
 NAME = os.environ.get("BOT_NICK_NAME")
-BOT =  = str(NAME) if NAME else "BLACK LIGHTNING"
+BOT =  = str(NAME) if NAME else "SAVAGE BOT"
 #op for keinshin
 
-Keinshin = "[KEINSHIN](https://t.me/keinshin)"
-OP = "[BLACK LIGHTNING](https://github.com/KeinShin/Black-Lightning)"
-OKAY = "[SUPPORT GROUP](https://t.me/LIGHTINGSUPPORT)"
+SAMEER = "[SAMEER](https://t.me/sameer_795)"
+OP = "[SAVAGE BOT](https://github.com/sameerpanthi/SAVAGE-IS-BACK)"
+OKAY = "[SUPPORT GROUP](https://t.me/savage_team_userbot)"
 #itna test h aur aage krte h
 #test successful raha ab aage 
-ALIVE = "BLACK LIGHTNING BOT IS ON 🔥 FIRE 🔥" #make by LEGENDX22 for black lightning
-USERBOT = " HELLO MASTER MY NAME IS BLACK LIGHTNING BOT I AM A BEST USERBOT 💝"
+ALIVE = "SAVAGE BOT IS ON🔥 FIRE 🔥" #make by LEGENDX22 for SAVAGE BOT
+USERBOT = " HELLO MASTER MY NAME IS SAVAGE BOT I AM A BEST USERBOT 💝"
 EMOJI = "⚡"
 #yrr isko apne bot me aply krne se pehle mere se pooch lena ok
 #aur aage add kruga abhi busy okay 🤔
