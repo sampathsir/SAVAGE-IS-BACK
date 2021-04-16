11ee3b677b1d51029351097bb677e104a0a1082c
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-04-16 11:14:48 +0530

    Update fuck.py

--- userbot/plugins/fuck.py
+++ userbot/plugins/fuck.py
@@ -21,7 +21,7 @@ async def _(event):
 
         return
 
-    animation_interval = 1
+    animation_interval = 20
 
     animation_ttl = range(0, 101)
 
@@ -79,7 +79,7 @@ async def _(event):
 
         return
 
-    animation_interval = 1
+    animation_interval = 5
 
     animation_ttl = range(0, 101)
 
